366bea66bf2fb79539a10b5229fd5494ac0beddc
Wyatt Greene <techiferous@gmail.com>
2010-07-17 16:42:11 -0400

    Added the first very basic test case.

--- test/linkify_test.rb
+++ test/linkify_test.rb
@@ -1,10 +1,18 @@
 require 'test/test_helper'
+require 'rack/mock'
 require 'rack-linkify'
 
 class LinkifyTest < Test::Unit::TestCase
 
-  def test_truth
-    assert true
+  def linkify_this_html(html)
+    app = lambda { |env| [200, {'Content-Type' => 'text/plain'}, html] }
+    response = Rack::MockRequest.new(app).get('/', :lint => true)
+  end
+
+  def test_basic_string
+    response = linkify_this_html('I like turtles')
+    assert_equal 200, response.status
+    assert_equal 'I like turtles', response.body
   end
 
 end
