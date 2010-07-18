541e1bd371ecaec9ba2c48588f7cbd8086a7e782
Wyatt Greene <techiferous@gmail.com>
2010-07-18 14:30:17 -0400

    Refactored test file into two test files, one for twitter links.

---
+++ test/twitter_links_test.rb
@@ -0,0 +1,9 @@
+require 'test/test_helper'
+
+class TwitterLinksTest < Test::Unit::TestCase
+
+  def test_stub
+    assert true
+  end
+
+end
--- test/urls_test.rb
+++ test/urls_test.rb
@@ -1,36 +1,7 @@
 require 'test/test_helper'
-require 'rack/mock'
-require 'rack-linkify'
-require 'dirb'
-require 'colored'
 
-class LinkifyTest < Test::Unit::TestCase
+class UrlsTest < Test::Unit::TestCase
 
-  def linkify_this_html(html)
-    app = lambda { |env| [200, {'Content-Type' => 'text/html'}, html] }
-    app2 = Rack::Linkify.new(app)
-    Rack::MockRequest.new(app2).get('/', :lint => true).body
-  end
-  
-  def assert_html_equal(expected, actual)
-    # Rack::Linkify uses Nokogiri under the hood, which does not
-    # preserve the same whitespace between tags when it processes
-    # HTML.  This means we can't do a simple string comparison.
-    # However, if we run both the expected and the actual
-    # through Nokogiri, then the whitespace will be changed in
-    # the same way and we can do a simple string comparison.
-    expected = Nokogiri::HTML(expected).to_html
-    actual = Nokogiri::HTML(actual).to_html
-    preamble = "\n"
-    preamble =  "*****************************************************\n"
-    preamble << "* The actual HTML does not match the expected HTML. *\n"
-    preamble << "* The differences are highlighted below.            *\n"
-    preamble << "*****************************************************\n"
-    message = preamble.magenta
-    message << Dirb::Diff.new(expected, actual).to_s(:color)
-    assert_block(message) { expected == actual }
-  end
-  
   def test_basic_document
     before_html = %Q{
       <!DOCTYPE html
