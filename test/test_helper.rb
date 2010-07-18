1dde2079ce49a702925110db2a892a94d5075e1f
Wyatt Greene <techiferous@gmail.com>
2010-07-18 16:12:38 -0400

    Moved test helpers out of Rack::Linkify and into Rack::Plastic.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,40 +1,19 @@
 require 'test/unit'
 require 'rubygems'
 require 'redgreen'
-require 'rack/mock'
 require 'rack-linkify'
-require 'dirb'
-require 'colored'
+require 'plastic_test_helper'
 
 module Test
   module Unit
     class TestCase
+      
+      include PlasticTestHelper
 
       def linkify_this_html(html)
-        app = lambda { |env| [200, {'Content-Type' => 'text/html'}, html] }
-        app2 = Rack::Linkify.new(app, :twitter => true)
-        Rack::MockRequest.new(app2).get('/', :lint => true).body
+        process_html(html, Rack::Linkify, :twitter => true)
       end
   
-      def assert_html_equal(expected, actual)
-        # Rack::Linkify uses Nokogiri under the hood, which does not
-        # preserve the same whitespace between tags when it processes
-        # HTML.  This means we can't do a simple string comparison.
-        # However, if we run both the expected and the actual
-        # through Nokogiri, then the whitespace will be changed in
-        # the same way and we can do a simple string comparison.
-        expected = Nokogiri::HTML(expected).to_html
-        actual = Nokogiri::HTML(actual).to_html
-        preamble = "\n"
-        preamble =  "*****************************************************\n"
-        preamble << "* The actual HTML does not match the expected HTML. *\n"
-        preamble << "* The differences are highlighted below.            *\n"
-        preamble << "*****************************************************\n"
-        message = preamble.magenta
-        message << Dirb::Diff.new(expected, actual).to_s(:color)
-        assert_block(message) { expected == actual }
-      end
-      
     end
   end
 end
